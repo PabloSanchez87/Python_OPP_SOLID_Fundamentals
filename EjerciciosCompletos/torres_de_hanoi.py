66191f2e95686a363cea501060388cdecb80eb1e
PabloSanchez <sancheztorrespablo@gmail.com>
2024-06-14 11:53:47 +0200

    Ejercicio resuelto Hanoi, mostrando por consola listas con los movimientos.

--- EjerciciosCompletos/torres_de_hanoi.py
+++ EjerciciosCompletos/torres_de_hanoi.py
@@ -22,3 +22,61 @@ desde la torre A hasta la torre C.
 - N de torres                           Mover disco de la torre A a la torre B
 - Torres : origen, desitno, auxiliar    …
 """
+
+# Versión inicial.
+"""
+def mover_disco(desde, hacia, disco):
+    print(f"Mover disco {disco} de la torre {desde} hacia la torre {hacia}")
+    
+
+def torres_de_hanoi(n, origen, destino, auxiliar):
+    # Caso base
+    if n==1:
+        mover_disco(origen, destino, n)
+        return
+    torres_de_hanoi(n-1, origen, auxiliar, destino)
+    mover_disco(origen, destino, n)
+    torres_de_hanoi(n-1,auxiliar,destino,origen)        
+    
+torres_de_hanoi(3,"Origen", "Destino", "Auxiliar")
+"""
+
+def inicializar_torres(n):
+    origen = list(range(n, 0, -1))          # Torre inicial con discos del 1 al n
+    auxiliar = []                           # Torre auxiliar vacía
+    destino = []                            # Torre destino vacía
+    torres = [origen, auxiliar, destino]
+    print(torres)
+    return torres
+
+def hanoi(n, origen, destino, auxiliar):
+    if n == 1:
+        # Movimiento directo del disco de origen a destino
+        destino.append(origen.pop())
+        # Imprimir estado actual de las torres
+        print(torres)
+        return
+    
+    # Mover n-1 discos de origen a auxiliar usando destino como auxiliar
+    hanoi(n-1, origen, auxiliar, destino)
+    # Mover el disco restante de origen a destino
+    destino.append(origen.pop())
+    # Imprimir estado actual de las torres
+    print(torres)
+    # Mover n-1 discos de auxiliar a destino usando origen como auxiliar
+    hanoi(n-1, auxiliar, destino, origen)
+
+
+while True:
+    try:
+        # Obtener el número de discos
+        n = (input("Introduzca el número de discos con los que desea jugar('S' para salir): "))
+        if n.lower() != "s":
+            # Inicializar las torres según el número de discos
+            torres = inicializar_torres(int(n))
+            # Llamar a la función hanoi para resolver el problema
+            hanoi(int(n), torres[0], torres[2], torres[1])
+        else:
+            break
+    except ValueError:
+        pass
